8858993ec8c3bf0543238676385501b0441a49cd
bib <bizb@yiche.com>
2021-09-14 17:53:36 +0800

    a9 图片加水印

---
+++ app/src/main/java/cn/xiaobei56/testprojectbzb/a9/AddWatermark.java
@@ -0,0 +1,24 @@
+package cn.xiaobei56.testprojectbzb.a9;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import cn.xiaobei56.testprojectbzb.R;
+
+/**
+ * Project Name: TestProjectBzb
+ * Package Name: cn.xiaobei56.testprojectbzb.a9
+ * Author: bzb
+ * Date: 2021/9/14
+ * Description:
+ */
+public class AddWatermark extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a9_activity_main);
+
+    }
+}
